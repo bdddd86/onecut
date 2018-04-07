56d7279f29a391f843a32477cba3db1c6f51b8c1
YoonJung2 <tdksia@gmail.com>
2018-04-08 00:13:34 +0900

    점프대 추가

---
+++ OneCut/Assets/Script/MushroomJumpObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomJumpObject : Gimmick {
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 20f), ForceMode2D.Impulse);
+        }
+    }
+}
