b42351fdc5bc28a0bf4219d0e717764ad9421e42
YoonJung2 <tdksia@gmail.com>
2018-07-22 00:33:54 +0900

    NPC 이름 버그 수정

--- OneCut/Assets/Script/NPCObject.cs
+++ OneCut/Assets/Script/NPCObject.cs
@@ -12,6 +12,14 @@ public class NPCObject : MonoBehaviour
         if (lblName)
         {
             lblName.text = npcName;
+        }
+    }
+
+    private void Update()
+    {
+        if (lblName)
+        {
+            // out 처리 필요
             lblName.transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, this.gameObject.transform.position);
         }
     }
