0b85bd3aaa1afff0748108f0676044b04d779c1a
bdddd86 <joonbumpark864@gmail.com>
2018-10-21 13:12:04 +0900

    ddd

---
+++ BasketBall/Assets/InputBall.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputBall : MonoBehaviour {
+
+	public Slider mSlider;
+
+	Vector3 firstTouchPos = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		mSlider.minValue = 0f;
+		mSlider.maxValue = 30f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		#if UNITY_EDITOR
+		if (Input.GetMouseButton (0)) {
+			//Debug.Log("GetMouseButton");
+			if (firstTouchPos == Vector3.zero)
+			{
+				//Debug.Log("입력");
+				firstTouchPos = Input.mousePosition;
+			}
+		}
+		else if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log("GetMouseButtonDown");
+			// 여기안들어옴.
+		}
+		else if (Input.GetMouseButtonUp (0)) {
+			//Debug.Log("GetMouseButtonUp");
+			if (firstTouchPos != Vector3.zero)
+			{
+				float distance = Vector3.Distance(firstTouchPos, Input.mousePosition);
+				//Debug.Log("Distance: "+distance.ToString());
+				if (distance > 10f)
+				{
+					float power = distance * 0.1f;
+					if (power <= 3f){
+						power = 3f;
+					}
+					if (power >= 30f){
+						power = 30f;
+					}
+					GetComponent<Rigidbody>().velocity = Vector3.zero;
+					GetComponent<Rigidbody>().AddForce(0,-power,0,ForceMode.Impulse);
+
+					mSlider.value = power;
+				}
+				firstTouchPos = Vector3.zero;
+			}
+		}
+		//Debug.Log("Velocity: "+GetComponent<Rigidbody>().velocity.ToString());
+		#else
+		#endif
+
+		transform.Rotate (Vector3.back, 5f);
+	}
+}
---
+++ BasketBall/Assets/WallPool.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallPool : MonoBehaviour {
+
+	List<GameObject> mListWall = new List<GameObject>();
+	float mfDelay = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < transform.childCount; i++) {
+			mListWall.Add (transform.GetChild (i).gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (mListWall != null && mListWall.Count > 0) {
+			mfDelay += Time.deltaTime;
+
+			if (mfDelay >= 3f) {
+				mfDelay = 0f;
+
+				for (int i = 0; i < mListWall.Count; i++) {
+					if (mListWall [i].activeSelf == false) {
+						mListWall [i].transform.localPosition = new Vector3 (15f, -Random.Range(0,25) * 0.5f, 0f);
+						mListWall [i].SetActive (true);
+						break;
+					}
+				}
+			}
+
+			for (int i = 0; i < mListWall.Count; i++) {
+				if (mListWall [i].activeSelf == false)
+					continue;
+				mListWall [i].transform.Translate (Vector3.left * Time.deltaTime * 5f);
+
+				if (mListWall [i].transform.localPosition.x <= -30f) {
+					mListWall [i].SetActive (false);
+					//mListWall [i].transform.localPosition = new Vector3 (15f, 0f, 0f);
+				}
+			}
+		}
+	}
+}
